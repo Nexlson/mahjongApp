678bb87d98dde70347dc5b90aff8b4342ba38282
Nexlson <nelsonchoon112@gmail.com>
2022-01-05 13:44:03 +0800

    first commit

---
+++ calculator/mahjong_calculator.py
@@ -0,0 +1,41 @@
+
+# winning = 8 points at least 
+# north 北, south 南，east 东， west 西 
+hold = []
+
+def arrangeHold():
+    pass
+
+def 大四喜(hold):
+    win = [["north", "north"]]
+    pass
+
+def winning_hands():
+    pass
+
+def mahjong_cal():
+    # 88 points
+
+    # 64 points
+
+    # 48 points 
+
+    # 32 points 
+
+    # 24 points 
+
+    # 16 points 
+
+    # 12 points 
+
+    # 8 points
+
+    # 6 points
+
+    # 4 points 
+
+    # 2 points 
+
+    # 1 point
+    pass
+    # return name, score
---
+++ calculator/tiles.py
@@ -0,0 +1,8 @@
+
+class tiles:
+    def __init__(self, pattern, value, name):
+        self.pattern = pattern
+        self.value = value
+        self.name = name
+        self.count = 4 
+
