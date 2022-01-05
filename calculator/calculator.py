c88b6ce604408ef423fe537e0904e634c9ba8819
Nexlson <nelsonchoon112@gmail.com>
2022-01-05 13:57:16 +0800

    update cal

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -2,6 +2,7 @@
 # winning = 8 points at least 
 # north 北, south 南，east 东， west 西 
 hold = []
+score = 0
 
 def arrangeHold():
     pass
@@ -14,28 +15,7 @@ def winning_hands():
     pass
 
 def mahjong_cal():
-    # 88 points
-
-    # 64 points
-
-    # 48 points 
-
-    # 32 points 
-
-    # 24 points 
-
-    # 16 points 
-
-    # 12 points 
-
-    # 8 points
-
-    # 6 points
-
-    # 4 points 
-
-    # 2 points 
-
-    # 1 point
+    
     pass
     # return name, score
+
--- calculator/tiles.py
+++
@@ -1,8 +0,0 @@
-
-class tiles:
-    def __init__(self, pattern, value, name):
-        self.pattern = pattern
-        self.value = value
-        self.name = name
-        self.count = 4 
-
