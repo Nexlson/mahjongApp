a77fb5a21b0765481f16c160ce42817db6cbfce7
Nexlson <nelsonchoon112@gmail.com>
2022-01-26 19:55:47 +0800

    update script

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # build images
-cd app/ && docker image build -t mahjongfront .
-cd ../core/ && docker image build -t mahjongback
+cd app/ && sudo docker image build -t mahjongfront .
+cd ../core/ && sudo docker image build -t mahjongback
 
 # run docker-compose on detach mode
-cd .. && docker-compose up -d
+cd .. && sudo docker-compose up -d
