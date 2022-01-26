df169784e9508bbc543e656ea223809ecadc759e
Nexlson <nelsonchoon112@gmail.com>
2022-01-26 17:48:16 +0800

    added detach mode to docker-compose script

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@
 cd app/ && docker image build -t mahjongfront .
 cd ../core/ && docker image build -t mahjongback
 
-# run docker-compose
-cd .. && docker-compose up
+# run docker-compose on detach mode
+cd .. && docker-compose up -d
